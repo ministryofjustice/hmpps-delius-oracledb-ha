db2b0ef3138abf4d296b2252a6633d4c00b8cf27
bill-buchan <bill.buchan@digital.justice.gov.uk>
2022-07-18 16:40:46 +0100

    Initial Revision

--- files/start_observing.sh
+++ files/start_observing.sh
@@ -2,12 +2,15 @@
 # Start Observing
 
 OBSERVER_CONFIG_FILE=$1
+CONNECT_IDENTIFIER=$2
+LOG_FILE=$3
+OBSERVER_FILE=$4
 
 . ~/.bash_profile
 
 dgmgrl <<EODG
 connect /
-set ObserverConfigFile=${OBSERVER_CONFIG_FILE}
-start observering;
+set ObserverConfigFile=${OBSERVER_CONFIG_FILE};
+start observer in background file is ${OBSERVER_FILE} logfile is ${LOG_FILE} connect identifier is ${CONNECT_IDENTIFIER};
 exit;
 EODG
