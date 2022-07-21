fd3145ec83b8fc9ea30b553b9d541c6ff850a274
bill-buchan <bill.buchan@digital.justice.gov.uk>
2022-07-21 15:55:13 +0100

    Line Terminator

--- files/get_observer_hosts.sh
+++ files/get_observer_hosts.sh
@@ -5,9 +5,8 @@ OBSERVER_CONFIG_FILE=$1
 
 . ~/.bash_profile
 
-dgmgrl <<EODG | awk '/^Observer .* - .*$/{OBSERVER=$2; TYPE=$NF};/Host Name:/{print OBSERVER,TYPE,$3}'
-connect /
-set ObserverConfigFile=${OBSERVER_CONFIG_FILE}
+dgmgrl /<<EODG | awk '/^Observer .* - .*$/{OBSERVER=$2; TYPE=$NF};/Host Name:/{print OBSERVER,TYPE,$3}'
+set ObserverConfigFile=${OBSERVER_CONFIG_FILE};
 show observers;
 exit;
 EODG
